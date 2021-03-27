4b2f67c2d0e698ce5c3054f42f1cec3c10f52189
Martin Michalik <xmicha82@vutbr.cz>
2021-03-27 16:49:06 +0100

    Socket test

---
+++ pythonSockTest/socketServer.py
@@ -0,0 +1,47 @@
+import socket
+import threading
+
+HEADER = 64
+PORT = 5050
+SERVER = socket.gethostbyname(socket.gethostname())
+ADDR = (SERVER, PORT)
+FORMAT = "utf-8"
+DC_MSG = "!DISCONNECT"
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind(ADDR)
+
+
+def handle_client(conn, addr):
+    print(f"[NEW CONNECTION] {addr} connected")
+
+    connected = True
+    while connected:
+        msg_length = conn.recv(HEADER).decode(FORMAT)
+        if msg_length:
+            msg_length = int(msg_length)
+            msg = conn.recv(msg_length).decode(FORMAT)
+
+            if msg == DC_MSG:
+                connected = False
+                print("[SYSTEM] Disconnecting")
+                break
+
+            print(f"[{addr}] {msg}")
+            conn.send("ACK".encode(FORMAT))
+
+    conn.close()
+
+
+def start():
+    server.listen()
+    print(f"[LISTENING] Server listening on {SERVER}")
+    while True:
+        conn, addr = server.accept()
+        thread = threading.Thread(target=handle_client, args=(conn, addr))
+        thread.start()
+        print(f"[ACTIVE CONNECTIONS] {threading.activeCount() - 1}")
+
+
+print(f"[STARTING] server is starting at port {PORT}")
+start()
---
+++ pythonSockTest/socketsClient.py
@@ -0,0 +1,28 @@
+import socket
+
+HEADER = 64
+PORT = 5050
+FORMAT = "utf-8"
+DC_MSG = "!DISCONNECT"
+SERVER = "127.0.1.1"
+ADDR = (SERVER, PORT)
+
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.connect(ADDR)
+
+
+def send(msg):
+    message = msg.encode(FORMAT)
+    msg_length = len(message)
+    send_length = str(msg_length).encode(FORMAT)
+    send_length += b" " * (HEADER - len(send_length))
+    client.send(send_length)
+    client.send(message)
+    print(client.recv(2048).decode(FORMAT))
+
+
+while True:
+    msg = input()
+    send(msg)
+    if msg == DC_MSG:
+        break
